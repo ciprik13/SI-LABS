b9ab3b3866fc3085a506add5539d6639d9eb56a4
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-02-21 17:32:08 +0200

    feat: implement app_lab_2_1 setup and loop functions

---
+++ src/app_lab_2_1/app_lab_2_1.cpp
@@ -0,0 +1,35 @@
+#include "app_lab_2_1.h"
+#include "srv_serial_stdio/srv_serial_stdio.h"
+#include "tasks/task_1.h"
+#include "tasks/task_2.h"
+#include "tasks/task_3.h"
+#include "dd_led/dd_led.h"
+#include "dd_button/dd_button.h"
+#include "srv_os_seq/srv_os_seq.h"
+
+
+void app_lab_2_1_setup() {
+    dd_led_setup();
+    dd_button_setup();
+    srv_serial_stdio_setup();
+    svr_os_seq_setup();
+    task_1_setup();
+    task_2_setup();
+    task_3_setup();
+    printf("App Lab 2.1: Started\n");
+}
+
+void app_lab_2_1_loop() {
+    int led_state = dd_led_is_on();
+
+    int led_blink_state = dd_led_1_is_on();
+
+    int blink_count = g_task3_blink_count;
+
+    printf("App Lab 2.1: Idle | LED1=%s | LED2(blink)=%s | BlinkCount=%d\n",
+           led_state      ? "ON"  : "OFF",
+           led_blink_state ? "ON"  : "OFF",
+           blink_count);
+
+    delay(1000);
+}
