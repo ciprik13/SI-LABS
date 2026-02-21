ef2a364007d9505b358e21fa7664e5910e5a0e60
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-02-21 18:02:39 +0200

    feat: update dd_led module to improve LED control and state management

--- src/dd_led/dd_led.h
+++ src/dd_led/dd_led.h
@@ -3,16 +3,22 @@
 
 #include <Arduino.h>
 
-#define LED_PIN LED_BUILTIN
+#define LED_PIN   13
 #define LED_1_PIN 12
 
 void dd_led_setup();
+
+void dd_led_set_target(int val);
+void dd_led_1_set_target(int val);
+
+void dd_led_apply();
+
+int dd_led_is_on();
+int dd_led_1_is_on();
+
 void dd_led_turn_on();
 void dd_led_turn_off();
 void dd_led_1_turn_on();
 void dd_led_1_turn_off();
-
-int dd_led_is_on();
-int dd_led_1_is_on();
 
 #endif
