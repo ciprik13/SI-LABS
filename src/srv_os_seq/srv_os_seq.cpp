89c999cad1b8fc8e2bcba87138b01b141993b927
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-02-22 23:50:57 +0200

    feat: update task definitions and timing for app_lab_3_1

--- src/srv_os_seq/srv_os_seq.cpp
+++ src/srv_os_seq/srv_os_seq.cpp
@@ -1,37 +1,28 @@
 #include "srv_os_seq/srv_os_seq.h"
-#include "app_lab_2_1/tasks/task_1.h"
-#include "app_lab_2_1/tasks/task_2.h"
-#include "app_lab_2_1/tasks/task_3.h"
-#include <stdio.h>
-
+#include "app_lab_3_1/tasks/task_1.h"
+#include "app_lab_3_1/tasks/task_2.h"
+#include "app_lab_3_1/tasks/task_3.h"
 #include "timer-api.h"
 
-int task_1_cnt = APP_LAB_2_1_TASK_1_OFFSET + APP_LAB_2_1_TASK_1_REC;
-int task_2_cnt = APP_LAB_2_1_TASK_2_OFFSET + APP_LAB_2_1_TASK_2_REC;
-int task_3_cnt = APP_LAB_2_1_TASK_3_OFFSET + APP_LAB_2_1_TASK_3_REC;
+int task_1_cnt = APP_LAB_3_1_TASK_1_OFFSET + APP_LAB_3_1_TASK_1_REC;
+int task_2_cnt = APP_LAB_3_1_TASK_2_OFFSET + APP_LAB_3_1_TASK_2_REC;
+int task_3_cnt = APP_LAB_3_1_TASK_3_OFFSET + APP_LAB_3_1_TASK_3_REC;
 
-void svr_os_seq_setup()
-{
+void svr_os_seq_setup() {
     timer_init_ISR_1KHz(TIMER_DEFAULT);
 }
 
-void timer_handle_interrupts(int timer)
-{
-    if (--task_1_cnt <= 0)
-    {
-        task_1_cnt = APP_LAB_2_1_TASK_1_REC;
+void timer_handle_interrupts(int timer) {
+    if (--task_1_cnt <= 0) {
+        task_1_cnt = APP_LAB_3_1_TASK_1_REC;
         task_1_loop();
     }
-
-    if (--task_2_cnt <= 0)
-    {
-        task_2_cnt = APP_LAB_2_1_TASK_2_REC;
+    if (--task_2_cnt <= 0) {
+        task_2_cnt = APP_LAB_3_1_TASK_2_REC;
         task_2_loop();
     }
-
-    if (--task_3_cnt <= 0)
-    {
-        task_3_cnt = APP_LAB_2_1_TASK_3_REC;
+    if (--task_3_cnt <= 0) {
+        task_3_cnt = APP_LAB_3_1_TASK_3_REC;
         task_3_loop();
     }
 }
--- src/srv_os_seq/srv_os_seq.h
+++ src/srv_os_seq/srv_os_seq.h
@@ -2,15 +2,16 @@
 #define SRV_OS_SEQ_H
 
 #define SRV_OS_SYS_TICK 1
-#define TIME_SEC        1000
+#define TIME_SEC 1000
 
-#define APP_LAB_2_1_TASK_1_REC    (100 / SRV_OS_SYS_TICK)   // 100ms
-#define APP_LAB_2_1_TASK_2_REC    (500 / SRV_OS_SYS_TICK)   // 500ms
-#define APP_LAB_2_1_TASK_3_REC    (100 / SRV_OS_SYS_TICK)   // 100ms
+// ---- Lab 3.1 timings ----
+#define APP_LAB_3_1_TASK_1_REC    1
+#define APP_LAB_3_1_TASK_2_REC    (100 / SRV_OS_SYS_TICK)
+#define APP_LAB_3_1_TASK_3_REC    (10 * TIME_SEC / SRV_OS_SYS_TICK)
 
-#define APP_LAB_2_1_TASK_1_OFFSET (0   / SRV_OS_SYS_TICK)   // start imediat
-#define APP_LAB_2_1_TASK_2_OFFSET (200 / SRV_OS_SYS_TICK)   // porneste dupa 200ms
-#define APP_LAB_2_1_TASK_3_OFFSET (400 / SRV_OS_SYS_TICK)   // porneste dupa 400ms
+#define APP_LAB_3_1_TASK_1_OFFSET 0
+#define APP_LAB_3_1_TASK_2_OFFSET (50  / SRV_OS_SYS_TICK)
+#define APP_LAB_3_1_TASK_3_OFFSET (200 / SRV_OS_SYS_TICK)
 
 extern int task_1_cnt;
 extern int task_2_cnt;
