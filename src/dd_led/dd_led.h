88ba5521ed1d3225f6d90a8b05ca44dedb58fe8a
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-02-22 23:50:50 +0200

    feat: add support for second LED in dd_led module

--- src/dd_led/dd_led.h
+++ src/dd_led/dd_led.h
@@ -3,18 +3,21 @@
 
 #include <Arduino.h>
 
-#define LED_PIN   13
-#define LED_1_PIN 12
+#define LED_PIN    13  // red
+#define LED_1_PIN  12  // green
+#define LED_2_PIN  11  // yellow
 
 void dd_led_setup();
 
 void dd_led_set_target(int val);
 void dd_led_1_set_target(int val);
+void dd_led_2_set_target(int val);
 
 void dd_led_apply();
 
 int dd_led_is_on();
 int dd_led_1_is_on();
+int dd_led_2_is_on();
 
 void dd_led_turn_on();
 void dd_led_turn_off();
