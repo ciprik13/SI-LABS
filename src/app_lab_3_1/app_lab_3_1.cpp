61abc9b8e4ba74d686bad48ffa7fc2f0f631bc8e
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-02-23 10:56:04 +0200

    refactor: reorder includes and standardize application setup definitions for clarity

--- src/app_lab_3_1/app_lab_3_1.cpp
+++ src/app_lab_3_1/app_lab_3_1.cpp
@@ -1,16 +1,16 @@
 #include "app_lab_3_1.h"
+#include "dd_led/dd_led.h"
+#include "dd_button/dd_button.h"
 #include "srv_serial_stdio/srv_serial_stdio.h"
+#include "srv_os_seq/srv_os_seq.h"
 #include "tasks/task_1.h"
 #include "tasks/task_2.h"
 #include "tasks/task_3.h"
-#include "dd_led/dd_led.h"
-#include "dd_button/dd_button.h"
-#include "srv_os_seq/srv_os_seq.h"
 
 void app_lab_3_1_setup() {
-    dd_led_setup();
-    dd_button_setup();
     srv_serial_stdio_setup();
+    dd_button_setup();
+    dd_led_setup();
     task_1_setup();
     task_2_setup();
     task_3_setup();
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,36 @@
 #include <Arduino.h>
-#include "app_lab_1_1/app_lab_1_1.h"
-#include "app_lab_1_2/app_lab_1_2.h"
-#include "app_lab_2_1/app_lab_2_1.h"
 #include "app_lab_3_1/app_lab_3_1.h"
+#include "app_lab_2_1/app_lab_2_1.h"
+#include "app_lab_1_2/app_lab_1_2.h"
+#include "app_lab_1_1/app_lab_1_1.h"
 
-#define USE_APP_LAB_1_1 11
-#define USE_APP_LAB_1_2 12
-#define USE_APP_LAB_2_1 21
-#define USE_APP_LAB_3_1 31
+#define LAB_1_1 11
+#define LAB_1_2 12
+#define LAB_2_1 21
+#define LAB_3_1 31
 
-#define APP_USED USE_APP_LAB_3_1
+#define ACTIVE_APP LAB_3_1
 
 void setup() {
-  #if APP_USED == USE_APP_LAB_1_1
-    app_lab_1_1_setup();
-  #elif APP_USED == USE_APP_LAB_1_2
-    app_lab_1_2_setup();
-  #elif APP_USED == USE_APP_LAB_2_1
-    app_lab_2_1_setup();
-  #elif APP_USED == USE_APP_LAB_3_1
+  #if ACTIVE_APP == LAB_3_1
     app_lab_3_1_setup();
+  #elif ACTIVE_APP == LAB_2_1
+    app_lab_2_1_setup();
+  #elif ACTIVE_APP == LAB_1_2
+    app_lab_1_2_setup();
+  #elif ACTIVE_APP == LAB_1_1
+    app_lab_1_1_setup();
   #endif
 }
 
 void loop() {
-  #if APP_USED == USE_APP_LAB_1_1
-    app_lab_1_1_loop();
-  #elif APP_USED == USE_APP_LAB_1_2
-    app_lab_1_2_loop();
-  #elif APP_USED == USE_APP_LAB_2_1
-    app_lab_2_1_loop();
-  #elif APP_USED == USE_APP_LAB_3_1
+  #if ACTIVE_APP == LAB_3_1
     app_lab_3_1_loop();
+  #elif ACTIVE_APP == LAB_2_1
+    app_lab_2_1_loop();
+  #elif ACTIVE_APP == LAB_1_2
+    app_lab_1_2_loop();
+  #elif ACTIVE_APP == LAB_1_1
+    app_lab_1_1_loop();
   #endif
 }
