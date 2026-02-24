67454af34638ad20990b143ef836798ae22b54fc
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-02-24 12:58:48 +0200

    feat: integrate app_lab_3_2 into main setup and loop functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,20 @@
 #include "app_lab_2_1/app_lab_2_1.h"
 #include "app_lab_1_2/app_lab_1_2.h"
 #include "app_lab_1_1/app_lab_1_1.h"
+#include "app_lab_3_2/app_lab_3_2.h"
 
 #define LAB_1_1 11
 #define LAB_1_2 12
 #define LAB_2_1 21
 #define LAB_3_1 31
+#define LAB_3_2 32
 
 #define ACTIVE_APP LAB_3_1
 
 void setup() {
-  #if ACTIVE_APP == LAB_3_1
+  #if ACTIVE_APP == LAB_3_2
+    app_lab_3_2_setup();
+  #elif ACTIVE_APP == LAB_3_1
     app_lab_3_1_setup();
   #elif ACTIVE_APP == LAB_2_1
     app_lab_2_1_setup();
@@ -24,7 +28,9 @@ void setup() {
 }
 
 void loop() {
-  #if ACTIVE_APP == LAB_3_1
+  #if ACTIVE_APP == LAB_3_2
+    app_lab_3_2_loop();
+  #elif ACTIVE_APP == LAB_3_1
     app_lab_3_1_loop();
   #elif ACTIVE_APP == LAB_2_1
     app_lab_2_1_loop();
