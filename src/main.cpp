f62ca6d7495b0bbb12a2ad8965e39fdb42d69d63
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-03-28 21:27:17 +0200

    feat: update ACTIVE_APP to LAB_5_2 and adjust setup/loop functions accordingly

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "app_lab_4_1/app_lab_4_1.h"
 #include "app_lab_4_2/app_lab_4_2.h"
 #include "app_lab_5_1/app_lab_5_1.h"
+#include "app_lab_5_2/app_lab_5_2.h"
+
 
 #define LAB_1_1 11
 #define LAB_1_2 12
@@ -16,11 +18,14 @@
 #define LAB_4_1 41
 #define LAB_4_2 42
 #define LAB_5_1 51
+#define LAB_5_2 52
 
-#define ACTIVE_APP LAB_5_1
+#define ACTIVE_APP LAB_5_2
 
 void setup() {
-  #if ACTIVE_APP == LAB_5_1
+  #if ACTIVE_APP == LAB_5_2
+    app_lab_5_2_setup();
+  #elif ACTIVE_APP == LAB_5_1
     app_lab_5_1_setup();
   #elif ACTIVE_APP == LAB_4_2
     app_lab_4_2_setup();
@@ -40,7 +45,11 @@ void setup() {
 }
 
 void loop() {
-  #if ACTIVE_APP == LAB_4_2
+  #if ACTIVE_APP == LAB_5_2
+    app_lab_5_2_loop();
+  #elif ACTIVE_APP == LAB_5_1
+    app_lab_5_1_loop();
+  #elif ACTIVE_APP == LAB_4_2
     app_lab_4_2_loop();
   #elif ACTIVE_APP == LAB_4_1
     app_lab_4_1_loop();
